94fd19f1c38b3184b947a58823b1a99bcc6917b4
hhyeok1026 <hhyeok1026@gmail.com>
2023-05-20 15:17:46 +0900

    create project

---
+++ app/src/main/java/com/hhyeok1026/fiftybuttons/Button1Activity.kt
@@ -0,0 +1,14 @@
+package com.hhyeok1026.fiftybuttons
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+/*
+i just make a one Button for first start!
+ */
+class Button1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_button1)
+    }
+}
